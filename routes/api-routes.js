71e2a13ee28b49af43927d03a80c9ac0fcf87c0b
Xandromus <xanderrapstine@gmail.com>
2018-05-05 21:11:47 -0500

    starting to set up api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,32 @@
+let db = require("../models");
+
+module.exports = function(app) {
+    
+app.get("/", function (req, res) {
+  
+});
+
+// API ROUTES
+
+// route to send added burger request through burger model function
+app.post("/api/burgers", function (req, res) {
+  
+});
+
+// route to display all burgers in database in a json
+app.get("/api/burgers", function (req, res) {
+  
+});
+
+// route to update status of burger through burger model function
+app.put("/api/burgers/:id", function (req, res) {
+
+
+});
+
+// route to delete burger through burger model function
+app.delete("/api/burgers/:id", function (req, res) {
+
+});
+
+};
